c0d7075796a1634f88b8b0f2f4334469a4a60e75
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-25 18:01:04 +0530

    Create Employee model

---
+++ EmployeeManagementSystemAPI/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagementSystemAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
