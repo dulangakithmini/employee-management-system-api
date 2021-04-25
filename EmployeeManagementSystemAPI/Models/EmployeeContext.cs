c02cc714727c276be92d569783ef2cefd4dcf476
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-25 18:09:48 +0530

    Create DB

---
+++ EmployeeManagementSystemAPI/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystemAPI.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
