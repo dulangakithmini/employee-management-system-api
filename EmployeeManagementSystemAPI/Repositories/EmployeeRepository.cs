afd00a267db608b0b47fc7ca59b5463ef266fae3
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-25 19:36:42 +0530

    Implement EmployeeRepository

---
+++ EmployeeManagementSystemAPI/Repositories/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EmployeeManagementSystemAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystemAPI.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeContext _employeeContext;
+
+        public EmployeeRepository(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+        }
+
+        public async Task<IEnumerable<Employee>> Get()
+        {
+            return await _employeeContext.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> Get(int id)
+        {
+            return await _employeeContext.Employees.FindAsync(id);
+        }
+
+        public async Task<Employee> Create(Employee employee)
+        {
+            _employeeContext.Employees.Add(employee);
+            await _employeeContext.SaveChangesAsync();
+
+            return employee;
+        }
+
+        public async Task Update(Employee employee)
+        {
+            _employeeContext.Entry(employee).State = EntityState.Modified;
+            await _employeeContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var employeeToDelete = await _employeeContext.Employees.FindAsync(id);
+            _employeeContext.Employees.Remove(employeeToDelete);
+            await _employeeContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ EmployeeManagementSystemAPI/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EmployeeManagementSystemAPI.Models;
+
+namespace EmployeeManagementSystemAPI.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> Get();
+
+        Task<Employee> Get(int id);
+
+        Task<Employee> Create(Employee employee);
+
+        Task Update(Employee employee);
+
+        Task Delete(int id);
+    }
+}
